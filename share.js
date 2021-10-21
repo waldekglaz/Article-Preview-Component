53b8dfbdc82d9724a578803413563ff14c01ccdf
waldekglaz <waldekglaz@gmail.com>
2021-10-21 22:18:55 +0100

    medias and js

--- share.js
+++ share.js
@@ -1,10 +1,11 @@
 const elShareBtn = document.querySelector('.share-btn');
 const elShareBox = document.querySelector('.share-box');
-const elShareBoxClose = document.querySelector('.close');
+// const elShareBoxClose = document.querySelector('.close');
 
 elShareBtn.addEventListener('click',() => {
-    elShareBox.classList.add('active');
-})
-elShareBoxClose.addEventListener('click', () =>{
-    elShareBox.classList.remove('active')
-})
+    elShareBox.classList.toggle('active');
+    elShareBtn.classList.toggle('active');
+});
+// elShareBoxClose.addEventListener('click', () =>{
+//     elShareBox.classList.remove('active')
+// })
